df3d5e61bc423eb082f9e464afec0bd12c162fb5
Ali324888 <zeeshank939@gmail.com>
2022-10-29 01:21:23 +0530

    spinner added

--- src/components/News.js
+++ src/components/News.js
@@ -1,22 +1,64 @@
 import React, { Component } from 'react'
+import Loading from './Loading';
 import NewsItem from './NewsItem'
 
+
 export class News extends Component {
+
+  constructor(){
+    super();
+    this.state = {
+      articles: [],
+      loading: false,
+      page: 1
+    }
+  }
+
+  async componentDidMount(){
+    let url = `https://newsapi.org/v2/top-headlines?country=in&category=business&apiKey=bd8765f393e74dcd8486bccea9c2458e&page=1&pageSize=${this.props.pageSize}`;
+    this.setState({loading: true});
+    let response = await fetch(url);
+    let data = await   response.json();
+    console.log(data);
+    this.setState({articles: data.articles, totalResults: data.totalResults, loading: false})
+  }
+
+  handlePrev = async () => {
+    let url = `https://newsapi.org/v2/top-headlines?country=in&category=business&apiKey=bd8765f393e74dcd8486bccea9c2458e&page=${this.state.page - 1}&pageSize=${this.props.pageSize}`;
+    this.setState({loading: true});
+    let response = await fetch(url);
+    let data = await   response.json();
+    console.log(data);
+    this.setState({articles: data.articles, page: this.state.page - 1, loading: false})
+  }
+
+  handleNext = async () => {
+    if(!(this.state.page > Math.ceil(this.state.totalResults/this.props.pageSize))){
+      let url = `https://newsapi.org/v2/top-headlines?country=in&category=business&apiKey=bd8765f393e74dcd8486bccea9c2458e&page=${this.state.page + 1}&pageSize=${this.props.pageSize}`;
+      this.setState({loading: true});
+      let response = await fetch(url);
+      let data = await   response.json();
+      console.log(data);
+      this.setState({articles: data.articles, page: this.state.page + 1, loading: false})
+    }
+    
+  }
+
   render() {
     return (
       <div className='container my-3'>
         <h1 className='title'>Daily news updates</h1>
-
+        {this.state.loading && <Loading/>}
         <div className="row my-2">
-            <div className="col-md-4">
-            <NewsItem/>
-            </div>
-            <div className="col-md-4">
-            <NewsItem/>
-            </div>
-            <div className="col-md-4">
-            <NewsItem/>
-            </div>
+          {!this.state.loading && this.state.articles.map((elem) => {
+            return <div className="col-md-4 my-2" key={elem.url}>
+                  <NewsItem title = {elem.title?elem.title: ""} description = {elem.description? elem.description: ""} url = {elem.url} imgUrl = {elem.urlToImage?elem.urlToImage:"https://images.livemint.com/img/2022/10/28/600x338/gallery-7_1666945754015_1666945761627_1666945761627.jpg"}/>
+                  </div>
+          })}  
+        </div>
+        <div className="container my-3 d-flex justify-content-between">
+        <button type="button" disabled={this.state.page <= 1} className="btn btn-dark" onClick={this.handlePrev}>&larr; Previous</button>
+        <button type="button" disabled={this.state.page >= Math.ceil(this.state.totalResults/this.props.pageSize)} className="btn btn-dark" onClick={this.handleNext}>Next &rarr;</button>
         </div>
       </div>
     )
--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -2,14 +2,15 @@ import React, { Component } from 'react'
 
 export class NewsItem extends Component {
   render() {
+  let {title, description, url, imgUrl} = this.props;
     return (
         <div>
             <div className="card" style={{width: "18rem"}}>
-                <img src="https://variety.com/wp-content/uploads/2022/10/albumcovertreatment-14.jpg?w=1000&h=562&crop=1" className="card-img-top" alt="..."/>
+                <img src={imgUrl} className="card-img-top" alt="..."/>
                 <div className="card-body">
-                    <h5 className="card-title">Rihanna Drops ‘Lift Me Up,’ Her First Single in Six Years, From ‘Black Panther: Wakanda Forever’ - Variety</h5>
-                    <p className="card-text">Rihanna dropped her first single in six years on Thursday night: “Lift Me Up,” the first track to emerge from Marvel’s blockbuster sequel “Black Panther: Wakanda Forever.” The song, a tribute to the life and legacy of Chadwick Boseman, was written by Nigerian…</p>
-                    <a href="/newsDescp" className="btn btn-primary">Read more</a>
+                    <h5 className="card-title">{title}</h5>
+                    <p className="card-text">{description}</p>
+                    <a href={url} rel="noreferrer" target="_blank" className="btn btn-dark">Read more</a>
                 </div>
             </div>
         </div>
